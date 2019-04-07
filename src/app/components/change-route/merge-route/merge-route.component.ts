334100f589a4049fcf4747b382a2222a03ffb2d4
Keerthi Annappa <keerthiannappa@gmail.com>
2019-04-07 12:13:31 +0530

    Apr 7
    check

--- src/app/components/change-route/merge-route/merge-route.component.ts
+++ src/app/components/change-route/merge-route/merge-route.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { BusService } from "src/app/services";
+import { ChangeRouteService } from "src/app/services/change-route/change-route.service";
+import { MatSnackBar } from "@angular/material";
+import sortjsonarray from "sort-json-array";
 
 @Component({
-  selector: 'app-merge-route',
-  templateUrl: './merge-route.component.html',
-  styleUrls: ['./merge-route.component.css']
+  selector: "app-merge-route",
+  templateUrl: "./merge-route.component.html",
+  styleUrls: ["./merge-route.component.css"]
 })
 export class MergeRouteComponent implements OnInit {
+  buses: any;
+  mergeRouteForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private busService: BusService,
+    private changeRouteService: ChangeRouteService,
+    private formBuilder: FormBuilder,
+    public snackBar: MatSnackBar
+  ) {}
 
   ngOnInit() {
+    this.fetchbuses();
+    this.mergeRouteForm = this.formBuilder.group({
+      bus_no:["",Validators.required],
+      route_name:["",Validators.required]
+    })
   }
 
+  get f(){
+    return this.mergeRouteForm.controls;
+  }
+
+  onSubmit(){
+    if(this.mergeRouteForm.invalid){
+      return;
+    }
+// should be filled
+  }
+
+  fetchbuses() {
+    this.busService.getAll().subscribe(res => {
+      res = sortjsonarray(res, "route_name");
+      this.buses = res;
+    });
+  }
 }
